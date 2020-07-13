48ebf546487ec2d67fd0c3de1f259a7b062f8da6
tjmccabe <tjmccabe3@gmail.com>
2020-07-13 14:42:58 -0500

    Enlarge center image and text

--- src/actor_frame.js
+++ src/actor_frame.js
@@ -69,7 +69,7 @@ class ActorFrame {
 
     this.sim = d3.forceSimulation()
       // .force("x", d3.forceX(this.width / 2).strength(.05))
-      .force("y", d3.forceY(this.height / 2).strength(.1))
+      .force("y", d3.forceY(this.height / 2).strength(.2))
       .force("charge", d3.forceManyBody().strength(-3000))
       .force("link", d3.forceLink().id(d => d.frameId))
       // .force("center", d3.forceCenter(this.width / 2, this.height / 2))
@@ -96,17 +96,26 @@ class ActorFrame {
       .attr("class", d => d.frameId[0])
       .attr("x", -25)
       .attr("y", -38)
-      .attr("height", 75)
       .attr("width", 50)
+      .attr("height", 75)
 
     var text = node
       .append("text")
       .attr("class", "nodetext")
       .attr("text-anchor", "middle")
       .attr("y", 50)
-      .attr("background-color", "rgba(255, 255, 255, 0.5)")
       .attr("font-size", 12)
       .text(d => d.text)
+
+    var cImage = images.filter((img, idx) => idx === 0)
+      .attr("x", -30)
+      .attr("y", -45)
+      .attr("width", 60)
+      .attr("height", 90)
+
+    var cText = text.filter((txt, idx) => idx === 0)
+      .attr("y", 58)
+      .attr("font-size", 13)
 
     this.sim
       .nodes(this.nodes)
--- src/movie_frame.js
+++ src/movie_frame.js
@@ -69,7 +69,7 @@ class MovieFrame {
 
     this.sim = d3.forceSimulation()
       // .force("x", d3.forceX(this.width / 2).strength(.05))
-      .force("y", d3.forceY(this.height / 2).strength(.1))
+      .force("y", d3.forceY(this.height / 2).strength(.2))
       .force("charge", d3.forceManyBody().strength(-3000))
       .force("link", d3.forceLink().id(d => d.frameId))
       // .force("center", d3.forceCenter(this.width / 2, this.height / 2))
@@ -89,7 +89,6 @@ class MovieFrame {
       .append("g")
       .attr("class", "node")
 
-
     var images = node
       .append("image")
       .attr("xlink:href", d => d.imgLink)
@@ -107,6 +106,16 @@ class MovieFrame {
       .attr("background-color", "rgba(255, 255, 255, 0.5)")
       .attr("font-size", 12)
       .text(d => d.text)
+
+    var cImage = images.filter((img, idx) => idx === 0)
+      .attr("x", -30)
+      .attr("y", -45)
+      .attr("width", 60)
+      .attr("height", 90)
+
+    var cText = text.filter((txt, idx) => idx === 0)
+      .attr("y", 58)
+      .attr("font-size", 13)
 
     this.sim
       .nodes(this.nodes)
